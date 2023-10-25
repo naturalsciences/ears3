6f14d36bdb0e65dfa65c141102007f31437b2ea8
Nils Van den Steen <nvandensteen@naturalsciences.be>
2023-10-25 16:04:27 +0200

    [FIX] remove unneeded lines

--- src/main/resources/static/js/settings-in-menu.js
+++ src/main/resources/static/js/settings-in-menu.js
@@ -13,10 +13,6 @@ if (window.localStorage && typeof window.localStorage !== 'undefined') {
             var me = JSON.parse(localStorage.actor);
             $("#settings-link").html(me.firstName + " " + me.lastName);
         }
-        initDropdowns(rdfBindings, selectedValues);
-        initSelectpicker('#programField');
-        initSelectpicker('#stationField');
-        initSelectpicker('#labelField');
     });
 } else {
     alert("Sorry, your browser does not support local storage.");
