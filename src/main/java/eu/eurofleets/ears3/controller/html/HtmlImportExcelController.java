1a2ed1da4a2c911b4401bbf3908b6f153481ee0b
jrymenans <jrymenans@naturalsciences.be>
2024-03-29 18:54:41 +0100

    Untested on ears-server, code to add empty template to the upload-excel ...

--- src/main/java/eu/eurofleets/ears3/controller/html/HtmlImportExcelController.java
+++ src/main/java/eu/eurofleets/ears3/controller/html/HtmlImportExcelController.java
@@ -2,11 +2,15 @@ package eu.eurofleets.ears3.controller.html;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.io.File;
+import java.io.IOException;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @Controller()
@@ -18,7 +22,14 @@ public class HtmlImportExcelController {
     @RequestMapping(method = {RequestMethod.GET}, value = {"/excel/import"}, produces = {"text/html; charset=utf-8"})
     public String event( Model model) {
         String maxFileSize = env.getProperty("spring.servlet.multipart.max-file-size");
+        File shipTemplate = null;
+        try {
+            shipTemplate = new ClassPathResource("static/ship_log_template.xlsx").getFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
         model.addAttribute("maxFileSize", maxFileSize);
+        model.addAttribute("shipTemplateURI", shipTemplate.toURI());
         return "excel-upload";
     }
 
